6b4a43d4ccf110641b8112e35cf1a05b5382fad1
denialexandre <deni.alexandre.garc@gmail.com>
2022-07-18 21:44:49 -0500

    versión1

---
+++ appmy/src/Components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return(
+        <nav>
+            <div>
+                Deni Alexandre's Store
+            </div>
+            <div>
+                <button>Apuntes de ITAM</button>
+                <button>Professional Services</button>
+                <button>Miscelaneous</button>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
